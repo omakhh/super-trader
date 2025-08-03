1fa99af75c944fe9c7e038362df0023f7258ceea
omakhh <opsdep@gmail.com>
2025-08-03 22:56:20 +0400

    Fix auto_updater script

--- auto_updater.py
+++ auto_updater.py
@@ -1,13 +1,16 @@
 import os
 import requests
 
-GITHUB_RAW_URL = ""
+GITHUB_RAW_URL = "https://raw.githubusercontent.com/omakhh/super-trader/main/super_trader.py"
 LOCAL_FILENAME = "super_trader.py"
 
 def download_update():
-    print("🔄 Downloading latest version of Super Machine...")
-    print("🟢 Running local version of Super Machine...")
-os.system("python super_trader.py")
+    print("🧠 Downloading latest version of Super Machine...")
+    r = requests.get(GITHUB_RAW_URL)
+    with open(LOCAL_FILENAME, 'wb') as f:
+        f.write(r.content)
+    print("🚀 Running local version of Super Machine...")
+    os.system("python super_trader.py")
 
 if __name__ == "__main__":
     download_update()
